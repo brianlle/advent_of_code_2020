4dcbf64c4ff74ac96f4855327d4a06fc9df23e7b
Brian Le <Brian.Le@ucsf.edu>
2020-12-22 00:32:57 -0800

    Update day22.R

--- code/day22.R
+++ code/day22.R
@@ -2,30 +2,26 @@
 # PART 1
 
 input <- read.delim("input/day22.txt", header = FALSE,
-                    blank.lines.skip = TRUE)
+                    blank.lines.skip = TRUE, stringsAsFactors = FALSE)
 
 library(collections)
+library(sets)
 
-
-p1 <- as.numeric(as.character(input$V1[2:26]))
-p2 <- as.numeric(as.character(input$V1[28:52]))
+p1 <- as.numeric(input$V1[2:26])
+p2 <- as.numeric(input$V1[28:52])
 
 while ((length(p1) > 0) & (length(p2) > 0)){
-  
   if (p1[1] > p2[1]){
     high_card <- max(p1[1], p2[1])
     low_card <- min(p1[1], p2[1])
-    
     p1 <- c(p1[-1], high_card, low_card)
     p2 <- p2[-1]
   } else if (p2[1] > p1[1]){
     high_card <- max(p1[1], p2[1])
     low_card <- min(p1[1], p2[1])
-    
     p1 <- p1[-1]
     p2 <- c(p2[-1], high_card, low_card)
   }
-  
 }
 
 sum <- 0
@@ -33,105 +29,101 @@ for (i in 1:length(p1)){
   sum <- sum + (length(p1)+1-i)*p1[i]
 }
 
-sum
-
-test <- c(3,2,10,6,8,5,9,4,7,1)
+print(sum)
 
 
 ### PART 2
 
-p1 <- as.numeric(as.character(input$V1[2:26]))
-p2 <- as.numeric(as.character(input$V1[28:52]))
-
-decks <- list(p1,p2)
-
-recursive_combat <- function(decks,i,j){
+recursive_combat <- function(decks,sub,existing_solutions){ # sub for printing subgame depth
   p1 <- decks[[1]]
   p2 <- decks[[2]]
-  not_first_time <- FALSE
-  p1_init <- decks[[1]]
-  p2_init <- decks[[2]]
+  
+  p1_init <- set() # sets of prior arrangements
+  p2_init <- set()
   
   while ((length(p1) > 0) & (length(p2) > 0)){
-    print(paste("Game", i, "Round", j))
-    
-    if(not_first_time){
-      # print("not first time")
-      # print(paste("p1:", p1))
-      # print(paste("p1i:", p1_init))
-      # print(paste("p2:", p2))
-      # print(paste("p2i:", p2_init))
-      if (identical(p1, p1_init) & identical(p2, p2_init)){
-        p1 <- c(0)
-        p2 <- c()
-        next
-      }
-    } else {
-      not_first_time <- TRUE
+    if ((paste(p1, collapse = ",") %in% p1_init) & (paste(p2, collapse = ",") %in% p2_init)){
+      p1 <- c(0)
+      p2 <- c()
+      decks <- list(p1,p2)
+      return(decks)
     }
 
-    if (p1[1] > (length(p1)-1) | p2[1]+1 > (length(p2)-1)){
+    p1_init <- c(p1_init, paste(p1, collapse = ","))
+    p2_init <- c(p2_init, paste(p2, collapse = ","))
+    
+    if (p1[1] > (length(p1)-1) | p2[1] > (length(p2)-1)){ # if no sub-game, high card wins
       if (p1[1] > p2[1]){
         high_card <- max(p1[1], p2[1])
         low_card <- min(p1[1], p2[1])
         p1 <- c(p1[-1], high_card, low_card)
         p2 <- p2[-1]
-        #decks <- list(p1, p2)
-        #decks <- recursive_combat(decks)
       } else if (p2[1] > p1[1]){
         high_card <- max(p1[1], p2[1])
         low_card <- min(p1[1], p2[1])
         p1 <- p1[-1]
         p2 <- c(p2[-1], high_card, low_card)
-        #decks <- list(p1, p2)
-        #decks <- recursive_combat(decks)
       }
-    } else {
+    } else { # initiate sub-game if a sub-game winner isn't already memoized
+      #print(paste("Going into game", i+1))
       p1_sub <- p1[2:(1+p1[1])]
       p2_sub <- p2[2:(1+p2[1])]
       decks_sub <- list(p1_sub, p2_sub)
-      decks_sub <- recursive_combat(decks_sub, i + 1, 1)
       
-      p1_sub <- decks_sub[[1]]
-      p2_sub <- decks_sub[[2]]
+      # memoizing by storing winners in a dict based on hands
+      hands_string <- paste(paste(p1, collapse = ","), paste(p2, collapse = ","), collapse = "")
       
-      if (length(p1_sub) == 0){
+      if (hands_string %in% existing_solutions$keys()){
+        winner <- existing_solutions$get(hands_string)
+        if (winner == 1){
+          p1_sub <- c(0)
+          p2_sub <- c()
+        } else if (winner == 2){
+          p1_sub <- c()
+          p2_sub <- c(0)
+        } else {
+          print("something's wrong")
+        }
+      } else {
+        decks_sub <- recursive_combat(decks_sub, i + 1, existing_solutions)
+        p1_sub <- decks_sub[[1]]
+        p2_sub <- decks_sub[[2]]
+      }
+
+      if (length(p1_sub) == 0){ #sub game winner is whoever's deck has 1+ cards
+        existing_solutions$set(hands_string, 2)
         winner <- p2[1]
         loser <- p1[1]
         p1 <- p1[-1]
         p2 <- c(p2[-1], winner, loser)
       } else if (length(p2_sub) == 0){
+        existing_solutions$set(hands_string, 1)
         winner <- p1[1]
         loser <- p2[1]
         p1 <- c(p1[-1], winner, loser)
         p2 <- p2[-1]
       } else {
-        print("fuck")
+        print("something else is wrong")
       }
     }
-    j <- j + 1
   }
   
   decks <- list(p1, p2)
   return(decks)
 }
 
-### test cases
-t1 <- c(12,14,16)
-t2 <- c(10,8,9)
-p1 <- c(24,1,16,7,40,19,47,31,46,30,50,2,28,21,42,36,34,15,48,10,45,6,22,9)
-p2 <- c(5,27,12,23,14)
+p1 <- as.numeric(input$V1[2:26])
+p2 <- as.numeric(input$V1[28:52])
+decks <- list(p1,p2)
+existing_solutions <- dict()
 
-recursive_combat(list(t1,t2))
+outcome <- recursive_combat(decks, 1, existing_solutions)
 
-recursive_combat(decks)
+outcome1 <- outcome[[1]]
 
-test1 <- c(9, 2, 6, 3, 1)
-test2 <- c(5, 8, 4, 7, 10)
-recursive_combat(list(test1, test2))
+sum <- 0
+for (i in 1:length(outcome1)){
+  sum <- sum + (length(outcome1)+1-i)*outcome1[i]
+}
 
-test3 <- c(43,19)
-test4 <- c(2,29,14)
-recursive_combat(list(test3, test4), 1, 1)
-
-outcome <- recursive_combat(list(p1, p2), 1, 1)
+print(sum)
