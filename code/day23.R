cc243468460013a06ae3e75645558d9b28dca5c6
Brian Le <Brian.Le@ucsf.edu>
2020-12-23 00:51:25 -0800

    Update day23.R

--- code/day23.R
+++ code/day23.R
@@ -256,3 +256,96 @@ print(Sys.time())
 print(cups$get(1) * cups$get(cups$get(1)))
 
 # 363807398885
+
+
+#### PART 2 even more bonus optimization
+# since we're only tracking a single value, we don't even need
+# to use a fake dict() that isn't native to R, we can use
+# arrays to track the next value!
+
+#### BONUS: part 2 optimization
+# keeping track of the *previous* cup value is unnessary, can
+# cut down operations by almost half by only keeping track of
+# the next cup value
+
+
+input <- "952316487"
+input <- as.numeric(strsplit(input, split = "")[[1]])
+input <- c(input, 10:1000000) # 1 million cups
+input_length <- length(input)
+
+#ten million plays
+plays_left <- 10000000
+current_cup <- 0
+
+# create an array that effectively is:
+# i is the cup value, array[i] stores the value of the *next* cup
+cups <- rep(0, 1000000)
+for (i in 1:9){
+  loc <- which(input == i)
+  cups[i] <- input[loc+1]
+}
+for (i in 10:(input_length-1)){
+  cups[i] <- input[i+1]
+}
+cups[1000000] <- input[1]
+
+current_cup <- input[1]
+
+print(Sys.time())
+# repeat 10,000,000 times
+while (plays_left > 0){
+  #print(plays_left)
+  
+  destination_cup <- as.numeric(current_cup)-1
+  
+  if (destination_cup == 0){ # loop around to end
+    destination_cup <- as.numeric(input_length)
+  }
+  
+  # identify next 3 cups to be moved; if the destination cup is any of these,
+  # update per rules by subtracting 1 as many times as needed
+  next1 <- cups[current_cup]
+  next2 <- cups[cups[current_cup]]
+  next3 <- cups[cups[cups[current_cup]]]
+  
+  while (destination_cup %in% c(next1, next2, next3)){
+    if (destination_cup == 1){
+      destination_cup <- as.numeric(input_length)
+    } else {
+      destination_cup <- destination_cup - 1
+    }
+  }
+  
+  #update original cup and new cup after
+  #effectively removing next1,next2,next3 from the chain
+  
+  cups[current_cup] <- cups[next3]
+  
+  
+  # next1,next2,next3 goes between destination_cup and destination_cup2
+  # update destination cup
+  destination_cup2 <- cups[destination_cup]
+  
+  cups[destination_cup] <- next1
+  
+  #check if destination_cup2 comes before next1
+  if (cups[destination_cup2] == next1){
+    cups[destination_cup2] <- cups[next3]
+  }
+  
+  #finally, update next1, next2, next3 cups
+  #next1 untouched, same next linking
+  #next2 untouched, same linkings in chain
+  cups[next3] <- destination_cup2
+  
+  ## next cup to start from is 1 after current up
+  broke_cup <- current_cup
+  current_cup <- cups[current_cup]
+  plays_left <- plays_left - 1
+}
+print(Sys.time())
+
+cups[1]*cups[cups[1]]
+
+# 363807398885
